839d06cc5924f2f7f2cedf3e1ab75f2f27187a16
Developer1 <developer1@gmail.com>
2021-09-10 06:22:25 +0100

    Updated crypt

---
+++ crypt.php
@@ -0,0 +1,30 @@
+<?php
+
+$arr = [
+    0xcf, 0x9c, 0xc2, 0xdf, 0x67, 0xa2, 0x24, 0x19,
+    0x2d, 0x2f, 0x88, 0xda, 0x20, 0x20, 0x00, 0xac,
+    0x94, 0xb9, 0x11, 0x45, 0x26, 0x51, 0x3a, 0x8f,
+    0xc0, 0x7c, 0xd2, 0x58, 0xcd, 0x7e, 0x50, 0x70,
+];
+
+
+$chars = array_map("chr", $arr);
+$bin = join($chars);
+$hex = bin2hex($bin);
+
+echo $hex . "\n";
+
+
+
+$hex2 = hex2bin('65da797fdc187de445cfc9f03e5d06e18a4b698933c9ef2ea34dcaadb439f0f3');
+
+$chars = array_map("ord", str_split($hex2));
+$chars = array_map("dechex", $chars);
+$chars = array_map(function($code) {
+   return  "0x" . $code;
+}, $chars);
+
+echo implode(", ", $chars) . "\n";
+
+
+//cf9cc2df67a224192d2f88da202000ac94b9114526513a8fc07cd258cd7e5070
